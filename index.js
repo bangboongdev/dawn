3d44e71672a0e73054ec3a3bef3824ded98b05a2
bangnv <bangnv@mic.vn>
2024-09-19 19:48:22 +0700

    update code

--- index.js
+++ index.js
@@ -1,192 +1,283 @@
 import fetch from "node-fetch";
-import fs from "fs";
-import readline from "readline";
-
-const keepaliveUrl =
-  "https://www.aeropres.in/chromeapi/dawn/v1/userreward/keepalive";
-const getPointsUrl =
-  "https://www.aeropres.in/api/atom/v1/userreferral/getpoint";
+import axios from "axios";
+import colors from "colors";
+import csvdb from "csv-database";
+import { HttpsProxyAgent } from "https-proxy-agent";
+import https from "https";
+import wordlist  from "wordlist-english";
+const englishWords = wordlist['english'];
+import crypto from "crypto";
+const discordUrl = "https://discord.com/api/v9/interactions";
 
 // Hiển thị thông báo chào mừng
-function displayWelcomeMessage() {
-  console.log(`
-       _ _                  _____    ___ 
-      (_) |                |  _  |  /   |
-  __ _ _| | __ _ _ __ __  _| |/' | / /| |
- / _\` | | |/ _\` | '_ \\\\ \\/ /  /| |/ /_| |
-| (_| | | | (_| | | | |>  <\\ |_/ /\\___  |
- \\__, |_|_|\\__,_|_| |_/_/\\_\\\\___/     |_/
-  __/ |                                  
- |___/                                   
-  `);
+// Function to generate a random 19-digit nonce
+function generateNonce() {
+  const randomBuffer = crypto.randomBytes(8); // Generate 8 random bytes
+  const randomBigInt = BigInt(`0x${randomBuffer.toString("hex")}`);
+
+  // Convert it to a number within the range of 19 digits
+  const nonce = randomBigInt % BigInt("10000000000000000000"); // Ensure it's within 19 digits
+  return nonce.toString().padStart(19, "0"); // Pad with leading zeros if necessary
 }
 
-// Đọc dữ liệu từ file
-async function readDataFile(filename) {
-  const accounts = [];
-  const fileStream = fs.createReadStream(filename);
+const getRandomWords = (n) => {
+  let randomWords = "";
 
-  const rl = readline.createInterface({
-    input: fileStream,
-    crlfDelay: Infinity,
-  });
-
-  for await (const line of rl) {
-    const [email, token] = line.split("|");
-    accounts.push({ email, token });
+  for (let i = 0; i < n; i++) {
+    const randomIndex = Math.floor(Math.random() * englishWords.length);
+    randomWords =englishWords[randomIndex];
   }
 
-  return accounts;
-}
+  return randomWords;
+};
 
-// Lấy tổng số điểm
-async function getTotalPoints(headers) {
-  try {
-    const response = await fetch(getPointsUrl, {
-      method: "GET",
-      headers,
-      insecureHTTPParser: true,
+class Oasis {
+  constructor(
+    application_id,
+    guild_id,
+    channel_id,
+    session_id,
+    proxiesConf = null,
+    access_token,
+    id
+  ) {
+    this.headers = {
+      accept: "*/*",
+      "accept-language": "en-US,en;q=0.9",
+      authorization: access_token,
+      origin: "https://discord.com",
+      pragma: "no - cache",
+      "sec-ch-ua-mobile": "?0",
+      "sec-ch-ua-platform": "Windows",
+      "sec-fetch-mode": "cors",
+      "user-agent":
+        "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/124.0.0.0 Safari/537.36",
+      "x-super-properties":
+        "eyJvcyI6IldpbmRvd3MiLCJicm93c2VyIjoiQ2hyb21lIiwiZGV2aWNlIjoiIiwic3lzdGVtX2xvY2FsZSI6ImVuLVVTIiwiYnJvd3Nlcl91c2VyX2FnZW50IjoiTW96aWxsYS81LjAgKFdpbmRvd3MgTlQgMTAuMDsgV2luNjQ7IHg2NCkgQXBwbGVXZWJLaXQvNTM3LjM2IChLSFRNTCwgbGlrZSBHZWNrbykgQ2hyb21lLzEyNC4wLjAuMCBTYWZhcmkvNTM3LjM2IiwiYnJvd3Nlcl92ZXJzaW9uIjoiMTI0LjAuMC4wIiwib3NfdmVyc2lvbiI6IjEwIiwicmVmZXJyZXIiOiJodHRwczovL3d3dy5nb29nbGUuY29tLyIsInJlZmVycmluZ19kb21haW4iOiJ3d3cuZ29vZ2xlLmNvbSIsInNlYXJjaF9lbmdpbmUiOiJnb29nbGUiLCJyZWZlcnJlcl9jdXJyZW50IjoiIiwicmVmZXJyaW5nX2RvbWFpbl9jdXJyZW50IjoiIiwicmVsZWFzZV9jaGFubmVsIjoic3RhYmxlIiwiY2xpZW50X2J1aWxkX251bWJlciI6MzI3NDQ2LCJjbGllbnRfZXZlbnRfc291cmNlIjpudWxsfQ==",
+      "Content-Type": "application/json",
+      Host: "discord.com",
+      Connection: "keep-alive",
+    };
+    // this.tele_data = tele_data;
+    this.access_token = access_token;
+    //Proxy
+    this.application_id = '1238009056451498024';
+    this.guild_id = guild_id;
+    this.channel_id = channel_id;
+    this.session_id = session_id;
+    this.proxies = proxiesConf;
+
+    this.agent = proxiesConf ? new HttpsProxyAgent(proxiesConf) : null;
+    this.id = id;
+  }
+
+  log(msg, type) {
+    const now = new Date();
+    const datetime = now.toLocaleString("en-US", {
+      year: "2-digit",
+      month: "2-digit",
+      day: "2-digit",
+      hour: "2-digit",
+      minute: "2-digit",
+      second: "2-digit",
+      hour12: false,
     });
-
-    if (response.ok) {
-      const jsonResponse = await response.json();
-      if (jsonResponse.status) {
-        const rewardPointData = jsonResponse.data.rewardPoint;
-        const referralPointData = jsonResponse.data.referralPoint;
-
-        const totalPoints =
-          (rewardPointData.points || 0) +
-          (rewardPointData.registerpoints || 0) +
-          (rewardPointData.signinpoints || 0) +
-          (rewardPointData.twitter_x_id_points || 0) +
-          (rewardPointData.discordid_points || 0) +
-          (rewardPointData.telegramid_points || 0) +
-          (rewardPointData.bonus_points || 0) +
-          (referralPointData.commission || 0);
-
-        return totalPoints;
-      } else {
-        console.log(
-          `\x1b[Error fetching points: ${
-            jsonResponse.message || "Unknown error"
-          }\x1b[0m`
-        );
-      }
-    } else if (response.status === 403) {
-      console.log(
-        `\x1b[Failed to retrieve points. Status code: ${response.status} - Forbidden. Token might be invalid or expired.\x1b[0m`
-      );
-    } else {
-      console.log(
-        `\x1b[Failed to retrieve points. Status code: ${response.status}\x1b[0m`
-      );
+    switch (type) {
+      case "success":
+        console.log(`[${datetime}] [*] ${this.id} ${msg}`.green);
+        break;
+      case "custom":
+        console.log(`[${datetime}] [*] ${this.id} ${msg}`);
+        break;
+      case "error":
+        console.log(`[${datetime}] [!] ${this.id} ${msg}`.red);
+        break;
+      case "warning":
+        console.log(`[${datetime}] [*] ${this.id} ${msg}`.yellow);
+        break;
+      default:
+        console.log(`[${datetime}] [*] ${this.id} ${msg}`.blue);
     }
-  } catch (error) {
-    console.log(
-      `\x1b[An error occurred while fetching points: ${error}\x1b[0m`
-    );
   }
-  return 0;
-}
 
-// Gửi yêu cầu keepalive
-async function makeRequest(headers, email) {
-  const keepalivePayload = {
-    username: email,
-    extensionid: "fpdkjdnhkakefebpekbdhillbhonfjjp",
-    numberoftabs: 0,
-    _v: "1.0.7",
-  };
+  async genaratePrompt() {
+    const englishWords = getRandomWords(1);
+    const body = {
+      type: 2,
+      application_id: this.application_id,
+      guild_id: this.guild_id,
+      channel_id: this.channel_id,
+      session_id: this.session_id,
+      data: {
+        version: "1274236506441121903",
+        id: "1260352671240425564",
+        name: "generate",
+        type: 1,
+        options: [
+          {
+            type: 3,
+            name: "prompt",
+            value: englishWords,
+          },
+        ],
+        application_command: {
+          id: "1260352671240425564",
+          type: 1,
+          application_id: this.application_id,
+          version: "1274236506441121903",
+          name: "generate",
+          description: "Generate imagery using Oasis' image generation model.",
+          options: [
+            {
+              type: 3,
+              name: "prompt",
+              description: "The prompt to use",
+              required: true,
+              min_length: 1,
+              max_length: 1000,
+              description_localized: "The prompt to use",
+              name_localized: "prompt",
+            },
+            {
+              type: 4,
+              name: "seed",
+              description: "Used to limit randomness.",
+              required: false,
+              min_value: 0,
+              max_value: 1000000,
+              description_localized: "Used to limit randomness.",
+              name_localized: "seed",
+            },
+            {
+              type: 4,
+              name: "guidance-scale",
+              description: "Used to determine how close output is to prompt.",
+              required: false,
+              min_value: 1,
+              max_value: 10,
+              description_localized:
+                "Used to determine how close output is to prompt.",
+              name_localized: "guidance-scale",
+            },
+          ],
+          dm_permission: true,
+          contexts: [0, 1, 2],
+          integration_types: [0, 1],
+          global_popularity_rank: 1,
+          description_localized:
+            "Generate imagery using Oasis' image generation model.",
+          name_localized: "generate",
+        },
+        attachments: [],
+      },
+      nonce: generateNonce(),
+      analytics_location: "slash_ui",
+    };
 
-  try {
-    const response = await fetch(keepaliveUrl, {
+    const options = {
+      headers: this.headers,
+      agent: this.agent,
       method: "POST",
-      headers,
-      body: JSON.stringify(keepalivePayload),
-      insecureHTTPParser: true,
+      maxBodyLength: Infinity,
+      data: JSON.stringify(body),
+      url: discordUrl,
+    };
+
+    try {
+      const response = await axios.request(options);
+      return response.status;
+    } catch (error) {
+      this.log(`An error occurred: ${error}`);
+      return false;
+    }
+  }
+
+  //Claim token
+  async runOasis() {
+    let delay = 24 * 60 * 60 * 1000;
+    try {
+      for (let index = 0; index < 200; index++) {
+        const random = randomSleep();
+        this.log("Sleep time " + random, "warning");
+        await sleep(random); 
+        const statusCode = await this.genaratePrompt();
+        statusCode == 204 ? this.log("Genarate Prompt Success: " + statusCode, "success") : this.log("Genarate Prompt Error: " + statusCode, "error"); ;
+      }
+    } catch (error) {
+      console.log("Error: " + error, "error");
+    }
+    this.log("Sleep time: " + delay / 1000 / 60, "warning");
+    return new Promise((resolve) => {
+      setTimeout(async () => {
+        await this.runOasis();
+      }, delay);
     });
-
-    console.log(`Status Code: \x1b[94m${response.status}\x1b[0m`);
-
-    if (response.ok) {
-      const jsonResponse = await response.json();
-      if (jsonResponse.message) {
-        console.log(`\x1b[Sukses: ${jsonResponse.message}\x1b[0m`);
-        return true;
-      } else {
-        console.log("\x1b[Message not found in response.\x1b[0m");
-      }
-    } else if (response.status === 403) {
-      console.log(
-        `\x1b[403 Forbidden. Email might be invalid for ${email}. Skipping...\x1b[0m`
-      );
-    } else if (response.status === 502) {
-      console.log("\x1b[502 Bad Gateway. Try again later...\x1b[0m");
-    }
-
-    return false;
-  } catch (error) {
-    console.log(`\x1b[An error occurred: ${error}\x1b[0m`);
-    return false;
   }
 }
 
-// Đếm ngược
-function countdown(seconds) {
-  let interval = setInterval(() => {
-    if (seconds > 0) {
-      process.stdout.write(
-        `\x1b[95mRestarting in: ${seconds} seconds\x1b[0m\r`
-      );
-      seconds--;
-    } else {
-      clearInterval(interval);
-      console.log("\n\x1b[Restarting the process...\x1b[0m\n");
-    }
-  }, 1000);
+function sleep(ms) {
+  return new Promise((resolve) => setTimeout(resolve, ms));
 }
 
-// Main logic
+async function runBot(record) {
+  const {
+    active,
+    access_token,
+    application_id,
+    guild_id,
+    channel_id,
+    session_id,
+    id,
+    proxy_url,
+  } = record;
+  const time = new Oasis(
+    application_id,
+    guild_id,
+    channel_id,
+    session_id,
+    proxy_url,
+    access_token,
+    id
+  );
+  // Chỉ chạy những trường hợp active =1
+  if (active == 1) {
+    await time.runOasis();
+  }
+}
+
+async function bot() {
+  const records = await db.get();
+  Promise.all(records.map((record) => runBot(record)))
+    .then(() => {
+      console.log("All operations completed");
+    })
+    .catch((error) => {
+      console.log("An error occurred during operation: ", error);
+    });
+}
+
+const db = await csvdb(
+  "oasis.csv",
+  [
+    "id",
+    "active",
+    "application_id",
+    "guild_id",
+    "channel_id",
+    "session_id",
+    "proxy_url",
+    "access_token",
+  ],
+  ","
+);
+
+function randomSleep(min = 125, max = 200) {
+  return Math.floor(
+    Math.random() * (max * 1000 - min * 1000 + 1000) + min * 1000
+  );
+}
+
 async function main() {
-  displayWelcomeMessage();
-
-  while (true) {
-    const accounts = await readDataFile("data.txt");
-    let totalAccumulatedPoints = 0;
-
-    for (const account of accounts) {
-      const { email, token } = account;
-
-      const headers = {
-        Accept: "*/*",
-        "Accept-Encoding": "gzip, deflate, br",
-        "Accept-Language": "en-US,en;q=0.9",
-        Authorization: `Bearer ${token}`,
-        "Content-Type": "application/json",
-        "User-Agent":
-          "Mozilla/5.0 (Linux; Android 10; K) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/120.0.0.0 Mobile Safari/537.36",
-      };
-
-      console.log(`\x1b[96mProcessing account: ${email}\x1b[0m`);
-
-      const points = await getTotalPoints(headers);
-      totalAccumulatedPoints += points;
-
-      const success = await makeRequest(headers, email);
-      if (success) {
-        console.log(`\x1b[Request for ${email} was successful.\x1b[0m\n`);
-      } else {
-        console.log(`\x1b[Request for ${email} failed.\x1b[0m\n`);
-      }
-    }
-
-    console.log(`\x1b[All accounts have been processed.\x1b[0m`);
-    console.log(
-      `\x1b[Total points from all users: ${totalAccumulatedPoints}\x1b[0m`
-    );
-
-    countdown(181);
-  }
+  await bot();
 }
-
 main();
